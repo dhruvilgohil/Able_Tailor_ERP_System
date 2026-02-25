56187fa4ebde50b1a42a2af37bf8e481d27fd5a5
dhruvilgohil <104555536+dhruvilgohil@users.noreply.github.com>
2026-02-25 08:45:32 +0530

    Initial commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Able_Tailor_ERP_System.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/Customers.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Able_Tailor_ERP_System.Controllers
+{
+    public class Customers : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
